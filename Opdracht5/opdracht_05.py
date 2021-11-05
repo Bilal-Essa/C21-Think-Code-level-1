c1a1f574c64de46999f19ebbffbb639d5b2af2d6
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2021-11-05 10:33:51 +0100

    eindopdracht think code

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,12 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamen
+
+def beste_games(game)
+    print("dit is mijn favoriete game")
+
+beste_games("fifa")
+beste_games("warzone")
+beste_games("call of duty")
+
+beste_games()
