f80e6d9e90ff196b27f483818a730c2cfac29b51
Bilal Essalhi <bilal.essalhi@student.rocva.nl>
2021-11-15 10:42:06 +0100

    done

---
+++ eindopdracht/eindopdracht.py
@@ -0,0 +1,18 @@
+print ("Eindopdracht van Bila")
+
+n = input ("Wat is jouw naam?")
+print ("Goedemiddag" + n )
+
+age = input ("Wat is jouw leeftijd?")
+print ("Dus jij bent " + age)
+
+print ("Jouw naam is " + n + "En je bent " + age +"jaar oud")
+
+H = input ("Wat zijn jouw hobby's?")
+print ("ooh" + H + "Dat is ook mijn hobby")
+
+C = input ("wat is je lievelings eten?")
+print ("Echt?!" + C + "dat lust ik niet")
+
+print ("We zijn gekomen bij het einde van mijn eindopdracht wat vond je ervan?") 
+K = input ()
